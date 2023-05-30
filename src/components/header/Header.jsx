15332787dbdbd62a95f00e37507b0cbf9afb222b
pitbrest <sasha.petrov@tut.by>
2023-05-31 00:18:51 +0300

    sc

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,36 +1,33 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import './Header.css';
 // import { Route, Routes, Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <Navbar expand="md">
-      <Container>
-        <Navbar.Brand href="/">Fake-Store</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="flex-grow-0">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Главная</Nav.Link>
-            <Nav.Link href="/about">О нас</Nav.Link>
-            <NavDropdown title="Каталог" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    // <Navbar expand="md">
+    //   <Container>
+    //     <Navbar.Brand href="/">Fake-Store</Navbar.Brand>
+    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //     <Navbar.Collapse id="basic-navbar-nav" className="flex-grow-0">
+    //       <Nav className="me-auto">
+    //         <Nav.Link href="/">Главная</Nav.Link>
+    //         <Nav.Link href="/about">О нас</Nav.Link>
+    //         <NavDropdown title="Каталог" id="basic-nav-dropdown">
+    //           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+    //           <NavDropdown.Item href="#action/3.2">
+    //             Another action
+    //           </NavDropdown.Item>
+    //           <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+    //           <NavDropdown.Divider />
+    //           <NavDropdown.Item href="#action/3.4">
+    //             Separated link
+    //           </NavDropdown.Item>
+    //         </NavDropdown>
+    //       </Nav>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
   );
 }
 
